23837276905b4d1bf611ecbfaf7ed66bec154a26
Paul Guan <paul.guan@epita.fr>
2024-10-24 01:20:41 +0200

    fix: Predict Model BERT fixed

--- model_app.py
+++ model_app.py
@@ -26,10 +26,23 @@ def predict_bert_model(size, rooms, garden):
     model, tokenizer = load_bert_model()
     text = f"This house is {size} square meters, has {rooms} bedrooms, and {'has' if garden == 1 else 'does not have'} a garden."
     inputs = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
-    with torch.no_grad():
-        outputs = model(**inputs)
-        predicted_price = outputs.logits.item()
-    return predicted_price
+    input_ids = inputs['input_ids']
+    attention_mask = inputs['attention_mask']
+    
+    # Inverse the Scaler to obtain the real result
+    df = pd.read_csv("./houses.csv")
+    y = df['price'].values.reshape(-1, 1)
+    scaler = MinMaxScaler()
+    scaler.fit(y)
+    
+    try : 
+        with torch.no_grad():
+            predicted_price = model(input_ids, attention_mask)
+        predicted_price = predicted_price.item()
+        predicted_price_actual = scaler.inverse_transform([[predicted_price]])[0][0]
+    except Exception as e:
+        st.write("Error : No data or data incorrect", e)
+    return predicted_price_actual
 
 def load_dashboard():
     house_size = st.number_input(
@@ -58,9 +71,7 @@ def load_dashboard():
     except Exception as e:
         st.write("Error : No data or data incorrect", e)
         
-def load_dashboard_bert_model():
-    model, tokenizer = load_bert_model()
-    
+def load_dashboard_bert_model():    
     house_size = st.number_input(
         "Insert size of the house", value=None, placeholder="Type a number...")
     
@@ -70,24 +81,9 @@ def load_dashboard_bert_model():
     house_garden = st.number_input(
         "Insert 1 to want a garden, else 0", value=None, placeholder="Type 1 or 0", min_value=0, max_value=1)
     
-    text = f"This house is {house_size} square meters, has {house_bedroom} bedrooms, and {'has' if house_garden == 1 else 'does not have'} a garden."
-    inputs = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
-    
-    input_ids = inputs['input_ids']
-    attention_mask = inputs['attention_mask']
-    
-    # Inverse the Scaler to obtain the real result
-    df = pd.read_csv("./houses.csv")
-    y = df['price'].values.reshape(-1, 1)
-    scaler = MinMaxScaler()
-    scaler.fit(y)
-    
     try : 
-        with torch.no_grad():
-            predicted_price = model(input_ids, attention_mask)
-        predicted_price = predicted_price.item()
-        predicted_price_actual = scaler.inverse_transform([[predicted_price]])[0][0]
-        st.write("Predictions Price", predicted_price_actual)
+        predict_price = predict_bert_model(house_size, house_bedroom, house_garden)
+        st.write("Predictions Price", predict_price)
     except Exception as e:
         st.write("Error : No data or data incorrect", e)
 
